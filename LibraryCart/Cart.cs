74dbcf9ee4621b400ad171ebf3290614df0b6e23
AkariYuu <v.v.rudchenko@student.khai.edu>
2025-05-24 00:39:31 +0300

    Add GetProducts to LibraryCart

--- LibraryCart/Cart.cs
+++ LibraryCart/Cart.cs
@@ -8,5 +8,14 @@ public class Cart : ICart
 
     public void AddProduct(IProduct product) => products.Add(product);
     public void RemoveProduct(IProduct product) => products.Remove(product);
-    public void Checkout() => Console.WriteLine("Order has been placed.");
+
+    public void Checkout()
+    {
+        if (products.Count == 0)
+            Console.WriteLine("Cart is empty. No order placed.");
+
+        Console.WriteLine($"Order has been placed with {products.Count} item(s).");
+    }
+
+    public IReadOnlyList<IProduct> GetProducts() => products.AsReadOnly();
 }
