45b16c17d77a09dab264d53d762baeb327f17ebf
AkariYuu <v.v.rudchenko@student.khai.edu>
2025-05-24 00:13:26 +0300

    Add projects that implements interfaces.

---
+++ LibraryCart/Cart.cs
@@ -0,0 +1,12 @@
+﻿using LibraryManagement;
+
+namespace LibraryCart;
+
+public class Cart : ICart
+{
+    private readonly List<IProduct> products = new();
+
+    public void AddProduct(IProduct product) => products.Add(product);
+    public void RemoveProduct(IProduct product) => products.Remove(product);
+    public void Checkout() => Console.WriteLine("Order has been placed.");
+}
