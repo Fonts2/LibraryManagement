eabec3caa110f990e3434869cfd72ec0f3ea37ca
Fonts2 <v.a.kovalov@student.khai.edu>
2025-05-24 11:52:39 +0300

    Change ICart and IPayment interfaces.

--- LibraryManagement/Interfaces/ICart.cs
+++ LibraryManagement/Interfaces/ICart.cs
@@ -4,5 +4,6 @@ public interface ICart
 {
     void AddProduct(IProduct product);
     void RemoveProduct(IProduct product);
-    void Checkout();
+    string Checkout();
+    IReadOnlyList<IProduct> GetProducts();
 }
